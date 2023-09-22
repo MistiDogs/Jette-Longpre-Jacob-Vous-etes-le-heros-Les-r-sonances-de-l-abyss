85c0029e2ccc4619242f59c27b0b3ed9b5683af6
MistiDogs <jacobjette111@gmail.com>
2023-09-22 17:56:33 -0400

    COMMENCEMENT DU CODE
    java

---
+++ script.js
@@ -0,0 +1,16 @@
+let chapters =
+    premierChapitre = {
+        titre: "titreDeMonPremierChapitre",
+        description: "descriptionDeMonPremierChapitre",
+        image: "imagepremier.jpeg",
+        option:[]
+    }
+
+;
+
+function goToChapter(cle) {
+
+   return  console.log(chapters[cle].titre),
+}
+
+//ÉCRIRE LE VRAI CONTENUE !
