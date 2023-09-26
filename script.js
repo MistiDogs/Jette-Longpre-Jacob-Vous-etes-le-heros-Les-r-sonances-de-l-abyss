a3745f2d7367ecf2daa7186aafa256d4ae8c6cf8
MistiDogs <112128007+MistiDogs@users.noreply.github.com>
2023-09-26 13:51:44 -0400

    continuation des chapitres js

--- script.js
+++ script.js
@@ -1,12 +1,70 @@
-let chapters =
-    premierChapitre = {
+let  =  {
+    arrive : {
+        titre: "Emménagement en ville",
+        description: "Vous êtes un détective qui a entendu des rumeurs d'une ville fantôme où surviennent des événements étranges, vous êtes venu enquêter.",
+        image: "",
+        option:[titre, direction]
+    },
+
+    premierChapitre : {
         titre: "titreDeMonPremierChapitre",
         description: "descriptionDeMonPremierChapitre",
         image: "imagepremier.jpeg",
         option:[]
-    }
+    },
 
-;
+    premierChapitre : {
+        titre: "titreDeMonPremierChapitre",
+        description: "descriptionDeMonPremierChapitre",
+        image: "imagepremier.jpeg",
+        option:[]
+    },
+
+    premierChapitre : {
+        titre: "titreDeMonPremierChapitre",
+        description: "descriptionDeMonPremierChapitre",
+        image: "imagepremier.jpeg",
+        option:[]
+    },  
+
+    premierChapitre : {
+        titre: "titreDeMonPremierChapitre",
+        description: "descriptionDeMonPremierChapitre",
+        image: "imagepremier.jpeg",
+        option:[]
+    },
+
+    premierChapitre : {
+        titre: "titreDeMonPremierChapitre",
+        description: "descriptionDeMonPremierChapitre",
+        image: "imagepremier.jpeg",
+        option:[]
+    },
+
+    premierChapitre : {
+        titre: "titreDeMonPremierChapitre",
+        description: "descriptionDeMonPremierChapitre",
+        image: "imagepremier.jpeg",
+        option:[]
+    },
+
+    premierChapitre : {
+        titre: "titreDeMonPremierChapitre",
+        description: "descriptionDeMonPremierChapitre",
+        image: "imagepremier.jpeg",
+        option:[]
+    },
+
+
+
+
+
+
+
+
+
+};
+
 
 function goToChapter(cle) {
 
