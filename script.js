ac34791242f32a43762c7d4274d5d691acb26b3a
MistiDogs <112128007+MistiDogs@users.noreply.github.com>
2023-09-29 17:43:09 -0400

    commencement js pour image princiap;e

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+const titre = document.querySelector('.titre');
+const description = document.querySelector('.description');
+const image = document.querySelector('.img');
+const video = document.querySelector('.video')
+
+//if else image sinon video, changer toute la ligne 
+
 let chapters =  {
     arrive : {
         titre: "Emménagement en ville",
         description: "Vous êtes un détective qui a entendu des rumeurs d'une ville fantôme où surviennent des événements étranges, vous êtes venu enquêter. Il y a deux endroits qui vous tapent à l'oeil.",
-        image: "./assets/images/arrive",
+        media: { type: 'img', path: "./assets/images/arrive.jpg"},
         option:[
             {titre: 'Poste de police | goToChapter("police")', 'destination': 'police'},
             {titre: 'Rue principale | goToChapter("principale")', 'destination': 'principale'}
@@ -12,7 +19,7 @@ let chapters =  {
     principale : {
         titre: "Sueurs froides",
         description: "Pendant votre promenade, vous êtes enveloppé dans une atmosphère intrigante où des murmures lointains et des ombres furtives éveillent votre curiosité. Vous envisagez de vous précipiter vers votre véhicule ou de poursuivre l'enquête.",
-        image: "./assets/images/principale",
+        media: { type: 'img', path: "./assets/images/principale.jpg"},
         option:[
             {titre: 'Continuer | goToChapter("bizarre")', 'destination': 'bizarre'},
             {titre: 'Se Replier | goToChapter("vide")', 'destination': 'vide'}
@@ -22,7 +29,7 @@ let chapters =  {
     police : {
         titre: "Centre de police",
         description: "Dans le centre de police, vous trouvez un pistolet sur un bureau, accompagné d'affiches de disparitions incomplètes.",
-        image: "./assets/images/police",
+        media: { type: 'img', path: "./assets/images/police.jpg"},
         option:[ 
             {titre: 'Presévérer | goToChapter("trouve")', 'destination': 'trouve'},
             {titre: 'Délaisser | goToChapter("principale")', 'destination': 'principale'}
@@ -32,21 +39,21 @@ let chapters =  {
     trouve : {
         titre: "Le Piège de l'Ombre Mortelle",
         description: "Vous abaissez vos gardes, relâchant votre vigilance un instant fatal, persuadé d'avoir trouvé refuge, et c'est alors qu'une créature surgit de l'ombre pour vous terrasser.",
-        image: "./assets/images/trouve",
+        media: { type: 'img', path: "./assets/images/trouve.jpg"},
         option:[{titre: 'Recommencer | goToChapter("arrive")', 'destination': 'arrive'}]
     },
 
     vide : {
         titre: "T'es pas toi même quand t'as faim",
         description: "Lors de votre fuite, vous perdez votre chemin dû au brouillard et vous finissez par mourir de faim.",
-        image: "./assets/images/vide",
+        media: { type: 'img', path: "./assets/images/vide.jpg"},
         option:[{titre: 'Recommencer | goToChapter("arrive")', 'destination': 'arrive'}]
     },  
 
     bizarre : {
         titre: "Entre chaos et l'imprévu",
         description: "Vous puisez votre courage et avancez d'un pas déterminé. Quelques instants plus tard, une créature énigmatique se révèle, figée dans une posture intrigante.",
-        image: "./assets/images/bizarre",
+        media: { type: 'img', path: "./assets/images/bizarre.jpg"},
         option:[
             {titre: 'Attendre | goToChapter("attendre")', 'destination': 'attendre'},
             {titre: 'Tirer | goToChapter("pow")', 'destination': 'pow'},
@@ -57,29 +64,33 @@ let chapters =  {
     attendre : {
         titre: "Surpise !",
         description: "La créature bondit avec une férocité déconcertante, vous laissant suffisamment de temps pour rejoindre votre voiture et la démarrer, mais elle vous poursuit toujours de près...",
-        image: "./assets/images/attendreEtLancer.mp4",
+        media: { type: 'video', path: "./assets/images/attendreEtLancer.mp4"},
         option:[{titre: 'Recommencer | goToChapter("arrive")', 'destination': 'arrive'}]
     },
 
     pow : {
         titre: "Explosion Sonore",
         description: "La créature s'effondre, cela déclenche l'arrivée d'une meute d'ombre volante qui vous submerge.",
-        image: "./assets/images/pow.mp4",
+        media: { type: 'video', path: "./assets/images/pow.mp4"},
         option:[{titre: 'Recommencer | goToChapter("arrive")', 'destination': 'arrive'}]
     },
 
     lancer : {
         titre: "Escapade",
         description: "La créature se précipite à la poursuite du pistolet, vous offrant une opportunité de fuir la ville. ",
-        image: "./assets/images/pasencoretrouvé",
+        media: { type: 'video', path: "./assets/images/pasencoretrouvé"},
         option:[{titre: 'Recommencer | goToChapter("arrive")', 'destination': 'arrive'}]
     },
 };
 
 
-let pistolOuiNon = false;
-
+goToChapter('bizarre');
 function goToChapter(chapterKey) {
+    
+    titre.innerHTML = chapters[chapterKey].titre;
+    description.innerHTML = chapters[chapterKey].description;
+    image.src = chapters[chapterKey].image;
+
     if (chapters[chapterKey]) {
         const chapter = chapters[chapterKey];
         console.log(chapter.titre);
@@ -93,5 +104,10 @@ function goToChapter(chapterKey) {
     }
 }
 
-goToChapter('arrive');
+
+
+
+
+
+
 
